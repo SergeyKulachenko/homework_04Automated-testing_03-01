7082ed3837a75edac6781c8499b0b7327bed3250
Sergey K <89184083452.mail.ru>
2020-10-05 20:38:48 +0300

    исправления

--- src/test/java/ru/netology/SelenideTest.java
+++ src/test/java/ru/netology/SelenideTest.java
@@ -10,28 +10,23 @@ import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.exactTextCaseSensitive;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SelenideTest {
 
     @Test
-    void selTestForm() {   //я знаю что заомиченный код это мусор, оставил как учебный материал!
-//        driver.get("http://localhost:9999");
+    void getTrueInputValidForm() {
         open("http://localhost:9999");
-//        WebElement form = driver.findElement(By.cssSelector("[class='form form_size_m form_theme_alfa-on-white']"));
         SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
-//        form.findElement(By.cssSelector("[type='text']")).sendKeys("Николай");
         form.$("[type=text]").setValue("Николай");
         form.$("[type=tel]").setValue("+79882223345");
-//        form.findElement(By.className("checkbox__box")).click();
         form.$("[class=checkbox__box]").click();
         form.$("[type=button]").click();
-//        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
-//        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
         $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
-    void selTestSelenideTry() {
+    void getTrueInputValidFormMethod2() {
         open("http://localhost:9999");
         $("[type='text']").setValue("Николай");
         $("[type='tel']").setValue("+79882223345");
@@ -39,7 +34,29 @@ public class SelenideTest {
         $("[type='button']").click();
         $("[data-test-id='order-success']").shouldHave
                 (exactTextCaseSensitive(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
 
+    @Test
+    void errorExpectedWhenEmptyFieldName() {
+        open("http://localhost:9999");
+        $("[type='text']").setValue("");
+        $("[type='tel']").setValue("+79882223345");
+        $("[class='checkbox__box']").click();
+        $("[type='button']").click();
+        $("[class=input__sub]").shouldHave
+                (exactTextCaseSensitive("Поле обязательно для заполнения"));
+
+    }
+
+    @Test
+    void errorExpectedWhenInputIncorrectName() {
+        open("http://localhost:9999");
+        $("[type='text']").setValue("Nikolay");
+        $("[type='tel']").setValue("+79882223345");
+        $("[class='checkbox__box']").click();
+        $("[type='button']").click();
+        $("[class='input__sub']").shouldHave
+                (exactTextCaseSensitive("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
 }
