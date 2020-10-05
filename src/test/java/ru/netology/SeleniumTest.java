ef2908f1ab8fcbd66037f2ea97f51231e1d15e56
Sergey K <89184083452.mail.ru>
2020-10-05 21:32:56 +0300

    проба настроек на Selenium

---
+++ src/test/java/ru/netology/SeleniumTest.java
@@ -0,0 +1,82 @@
+package ru.netology;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SeleniumTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "driver/linux/chromedriver");
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void getTrueInputValidForm() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Николаев Николай");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79882223345");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.cssSelector("[type='button']")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    @Test
+    void errorExpectedWhenInputIncorrectName() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Nikolay");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79882223345");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.cssSelector("[type='button']")).click();
+        String text = driver.findElement(By.cssSelector("[class='input__sub']")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+
+    @Test
+    void getTrueInputValidFormMethod2() {
+        driver.get("http://localhost:9999");
+        WebElement form = driver.findElement(By.cssSelector("[class='form form_size_m form_theme_alfa-on-white']"));
+        form.findElement(By.cssSelector("[type='text']")).sendKeys("Николаев Николай");
+        form.findElement(By.cssSelector("[type='tel']")).sendKeys("+79882223345");
+        form.findElement(By.className("checkbox__box")).click();
+        form.findElement(By.cssSelector("[type='button']")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    @Test
+    void getTrueInputValidFormMethod3() {
+        driver.get("http://localhost:9999");
+        List<WebElement> webElements = driver.findElements(By.className("input__control"));
+        webElements.get(0).sendKeys("Николаев Николай");
+        webElements.get(1).sendKeys("+79882223345");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.cssSelector("[type='button']")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+}
